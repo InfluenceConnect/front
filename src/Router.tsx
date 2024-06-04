28a3f8b23bc3dadf075bfb6e557d584f659c8886
ajpf44 <ajpf44@gmail.com>
2024-06-04 16:35:48 -0300

    wip: começa a desenvolver a barra de acessibilidade

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import LoginPage from "./pages/login/Login";
 import Footer from "./components/Footer";
+import AccessibilityBar from "./components/AccessibilityBar";
 
 const Router = ()=>{
 
     return(
         <BrowserRouter>
+            <AccessibilityBar/>
             <Header />
             <Routes>
                 <Route path="/" element={<LoginPage/>} />
