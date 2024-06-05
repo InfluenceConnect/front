15b2ae7fbb8d52b6d012c0e323c2725166cc51fa
VirLustitiae <zuvanovm@gmail.com>
2024-06-05 18:49:42 -0300

    criacao da pagina de status do cliente , adicionei a rotae criei a pagina, ainda sem api e sem a funcao de bloqueio

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Header from "./components/Header";
 import LoginPage from "./pages/login/Login";
 import Footer from "./components/Footer";
 import AccessibilityBar from "./components/AccessibilityBar";
 import RegisterNicheCompany from "./pages/company/register/RegisterNiche";
 import AccessibilityDrawer from "./components/AccessibilityDrawer";
-
 import { Container, CssBaseline } from "@mui/material";
 import RegisterNicheInfluencer from "./pages/influencer/register/RegisterNiche";
+import AccountStatus from "./pages/login/AccountStatus"; //rota adiciona pelo zuvanov para account status
 
 const Router = () => {
   return (
@@ -20,14 +19,9 @@ const Router = () => {
       <Container sx={{minHeight: "69vh"}}>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/registerNicheCompany"
-            element={<RegisterNicheCompany />}
-          />
-           <Route
-            path="/registerNicheInfluence"
-            element={<RegisterNicheInfluencer />}
-          />
+          <Route path="/registerNicheCompany" element={<RegisterNicheCompany />} />
+          <Route path="/registerNicheInfluence" element={<RegisterNicheInfluencer />} />
+          <Route path="/accountStatus" element={<AccountStatus />} /> {/* Nova rota  adiciona pelo zuvanov, posteriormente aqui ira bloquear as outras rotas  caso o status diga*/}
           <Route path="*" element={<h1>not found</h1>} />
         </Routes>
       </Container>
