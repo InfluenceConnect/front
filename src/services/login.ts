fa263bc57dfd217d86111a0f037af0473c1360c1
Rodrigo Costa <rodrigo.silvacostalc@gmail.com>
2024-06-19 14:38:06 -0300

    Adição de pequeno comentario arquivo login, diretório service

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,14 +1,17 @@
 import api from "./api";
 
-const login = async(email: string, password: string) =>{
+// Função assíncrona para login do usuário
+const login = async (email: string, password: string) => {
   try {
-    const req = {email: email, password: password}
-    const res = await api.post("/users/login",req)
+    const req = { email: email, password: password };
+    const res = await api.post("/users/login", req);
+
     console.log(res);
-    return res.data
+
+    return res.data;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-export {login}
+export { login };
