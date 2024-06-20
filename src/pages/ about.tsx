adb1b2b2e5413a62535e7aaca7343e37f2b3e8e8
Rodrigo Costa <rodrigo.silvacostalc@gmail.com>
2024-06-20 14:11:04 -0300

    Mudanças page about includo Tab

--- src/pages/ about.tsx
+++ src/pages/ about.tsx
@@ -1,36 +1,53 @@
+import { useState } from 'react';
+import { Container, Typography, Box, useMediaQuery, useTheme, Tabs, Tab, Theme } from '@mui/material';
 
-import { Container, Typography, Box, useMediaQuery, useTheme } from '@mui/material';
+interface AboutProps {}
 
-
-  
-       
-
-
-
- const  About = () => {
-  const theme = useTheme();
+const About: React.FC<AboutProps> = () => {
+  const theme = useTheme<Theme>();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (_: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-        <Container>
+    <Container>
       <Box mt={isMobile ? 2 : 5} mb={isMobile ? 2 : 5}>
         <Typography variant="h3" gutterBottom>
           Sobre Nós
         </Typography>
-        <Typography variant="body1" textAlign={'justify'} paragraph>
-          Este site foi desenvolvido como um requisito da empresa Info4, com o objetivo de conectar influenciadores e empresas através de campanhas. Nosso projeto é voltado para criar um ambiente onde influenciadores podem encontrar oportunidades de trabalho e empresas podem encontrar os influenciadores ideais para suas campanhas de marketing.
-        </Typography>
-        <Typography variant="body1" textAlign={'justify'} paragraph>
-          A Info4, uma empresa comprometida com a inovação e soluções tecnológicas, identificou a necessidade de uma plataforma eficiente que facilitasse a comunicação e colaboração entre influenciadores e empresas. A partir dessa visão, o Influence Connect foi criado para atender a essas demandas, seguindo uma série de regras e requisitos rigorosos para garantir seu funcionamento correto e seguro.
-        </Typography>
-        <Typography variant="body1" textAlign={'justify'} paragraph>
-          Nosso objetivo é fornecer uma plataforma robusta e segura que atenda às necessidades de nossos usuários, promovendo conexões valiosas e ajudando a impulsionar campanhas de marketing eficazes. Acreditamos no poder da colaboração e estamos dedicados a oferecer a melhor experiência possível para nossos usuários.
-        </Typography>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant={isMobile ? 'scrollable' : 'standard'}
+          scrollButtons="auto"
+        >
+          <Tab label="Objetivo" aria-label="Objetivo da empresa Info4"  tabIndex={0}/>
+          <Tab label="Visão da Empresa" aria-label="Visão da empresa Info4" tabIndex={0} />
+          <Tab label="Nossa Missão" aria-label="Missão da empresa Info4"  tabIndex={0}/>
+        </Tabs>
+        <Box mt={2}>
+          {value === 0 && (
+            <Typography variant="body1" textAlign={'justify'} paragraph tabIndex={0}>
+              Este site foi desenvolvido como um requisito da empresa Info4, com o objetivo de conectar influenciadores e empresas através de campanhas. Nosso projeto é voltado para criar um ambiente onde influenciadores podem encontrar oportunidades de trabalho e empresas podem encontrar os influenciadores ideais para suas campanhas de marketing.
+            </Typography>
+          )}
+          {value === 1 && (
+            <Typography variant="body1" textAlign={'justify'} paragraph tabIndex={0}>
+              A Info4, uma empresa comprometida com a inovação e soluções tecnológicas, identificou a necessidade de uma plataforma eficiente que facilitasse a comunicação e colaboração entre influenciadores e empresas. A partir dessa visão, o Influence Connect foi criado para atender a essas demandas, seguindo uma série de regras e requisitos rigorosos para garantir seu funcionamento correto e seguro.
+            </Typography>
+          )}
+          {value === 2 && (
+            <Typography variant="body1" textAlign={'justify'} paragraph tabIndex={0}>
+              Nosso objetivo é fornecer uma plataforma robusta e segura que atenda às necessidades de nossos usuários, promovendo conexões valiosas e ajudando a impulsionar campanhas de marketing eficazes. Acreditamos no poder da colaboração e estamos dedicados a oferecer a melhor experiência possível para nossos usuários.
+            </Typography>
+          )}
+        </Box>
       </Box>
     </Container>
   );
 };
 
 export default About;
-
-   
--- src/pages/policyPrivacy.tsx
+++ src/pages/policyPrivacy.tsx
@@ -1,50 +1,49 @@
-
 import { Container, Typography, Box, useMediaQuery, useTheme } from '@mui/material';
 
- const PrivacyPolicy = () => {
+const PrivacyPolicy = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Container>
+    <Container >
       <Box mt={isMobile ? 2 : 5} mb={isMobile ? 2 : 5}>
-        <Typography variant="h3" gutterBottom>
+        <Typography variant="h3" gutterBottom tabIndex={0}>
           Política de Privacidade
         </Typography>
-        <Typography variant="body1" textAlign={'justify'}paragraph>
+        <Typography variant="body1" textAlign={'justify'} paragraph tabIndex={0}>
           Bem-vindo ao Influence Connect. Nós levamos sua privacidade a sério e esta política explica como coletamos, usamos, e protegemos suas informações.
         </Typography>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom tabIndex={0}>
           Informações que coletamos
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Coletamos informações que você nos fornece diretamente, como quando você cria uma conta, atualiza seu perfil, ou entra em contato conosco. Também coletamos informações automaticamente sobre sua interação com nosso site.
         </Typography>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom tabIndex={0}>
           Como usamos suas informações
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Usamos suas informações para fornecer, manter e melhorar nossos serviços, além de comunicar com você sobre atualizações, ofertas e outras informações relevantes.
         </Typography>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom tabIndex={0}>
           Compartilhamento de informações
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Não compartilhamos suas informações pessoais com terceiros, exceto quando necessário para fornecer nossos serviços ou quando exigido por lei.
         </Typography>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom tabIndex={0}>
           Segurança
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Implementamos medidas de segurança para proteger suas informações contra acesso, uso ou divulgação não autorizados.
         </Typography>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom tabIndex={0}>
           Alterações nesta política
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Podemos atualizar esta política de privacidade periodicamente. Notificaremos você sobre quaisquer alterações publicando a nova política em nosso site.
         </Typography>
-        <Typography variant="body1" paragraph textAlign={'justify'}>
+        <Typography variant="body1" paragraph textAlign={'justify'} tabIndex={0}>
           Se você tiver alguma dúvida sobre nossa política de privacidade, entre em contato conosco.
         </Typography>
       </Box>
@@ -53,5 +52,3 @@ import { Container, Typography, Box, useMediaQuery, useTheme } from '@mui/materi
 };
 
 export default PrivacyPolicy;
-
-   
